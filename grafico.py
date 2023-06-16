be82766a4edf4d7e6e3b5aa6fff55d2a5dcb0fbf
SaikoReyes <72665381+SaikoReyes@users.noreply.github.com>
2023-06-16 01:56:10 -0600

    Interfaz gráfica para ingresar datos completada

--- grafico.py
+++ grafico.py
@@ -3,29 +3,10 @@ import matplotlib.pyplot as plt
 from scipy.optimize import linprog
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 
 restricciones_comp=[]
 restricciones_graph = []
-
-def ingresar_datos():
-    vnt = tk.Tk()
-    vnt.title("Ingresar datos")
-    vnt.geometry("400x600")
-
-    etiqueta = tk.Label(vnt, text = "¿Cuantas restricciones tienes?", font=("Arial", 18))
-    etiqueta.pack(pady=10)
-    entry = ttk.Entry()
-    entry.place(x=50, y=50)
-
-        
-    button = ttk.Button(text="Obtener texto")
-    button.place(x=50, y=100)
-    
-
-    
-
-    vnt.mainloop()
-    
 
 def mostrar_resultado(punto_optimo, valor_optimo):
     ventana = tk.Tk()
@@ -46,31 +27,25 @@ def mostrar_resultado(punto_optimo, valor_optimo):
 
     ventana.mainloop()
 
-
-
 def graficar_restricciones(tipo, valores, xlim, ylim, solucion, restricciones_comp):
 
-    
     x = np.linspace(xlim[0], xlim[1], 500)
     y = np.linspace(ylim[0], ylim[1], 500)
     X, Y = np.meshgrid(x, y)
 
     fig, ax = plt.subplots()
-    #ax.margins(x=0.05, y=0.05)  # Ajusta los márgenes en un 5% del rango de datos
     plt.draw()
 
-    # Crear una matriz de booleanos para almacenar las áreas factibles
     factible = np.ones_like(X, dtype=bool)
 
-    restriccion_x = [0, 1]  # Ecuación de la línea del eje X: x = 0
-    restriccion_y = [1, 0]  # Ecuación de la línea del eje Y: y = 0
+    restriccion_x = [0, 1]
+    restriccion_y = [1, 0]
 
-    ax.plot([0, 0], ylim, color='blue', linestyle='dashed')  # Graficar línea del eje Y
-    ax.plot(xlim, [0, 0], color='blue', linestyle='dashed')  # Graficar línea del eje X
+    ax.plot([0, 0], ylim, color='blue', linestyle='dashed')
+    ax.plot(xlim, [0, 0], color='blue', linestyle='dashed')
     plt.draw()
     plt.pause(1)
 
-    # Actualizar el área factible considerando las líneas de los ejes X e Y como restricciones
     factible = np.logical_and(factible, restriccion_x[0] * X + restriccion_x[1] * Y >= 0)
     factible = np.logical_and(factible, restriccion_y[0] * X + restriccion_y[1] * Y >= 0)
 
@@ -84,18 +59,17 @@ def graficar_restricciones(tipo, valores, xlim, ylim, solucion, restricciones_co
         plt.draw()
         plt.pause(1)
 
-    # Rellenar el área factible con un color diferente
     ax.imshow(factible, extent=(xlim[0], xlim[1], ylim[0], ylim[1]), origin='lower', alpha=0.3, cmap='Greens')
     
 
     plt.draw()
 
     plt.pause(1)
-    # Calcular el centro del área factible
+
     x_center = solucion[0]
     y_center = solucion[1]
 
-    #ablecer los límites de los ejes X e Y alrededor del centro del área factible
+
     ax.set_xlim(x_center - 20, x_center + 20)
     ax.set_ylim(y_center - 20, y_center + 20)
     
@@ -103,7 +77,7 @@ def graficar_restricciones(tipo, valores, xlim, ylim, solucion, restricciones_co
     plt.ylabel('y')
    
     plt.text(solucion[0], solucion[1],solucion)
-    # Plot feasible solution point
+
     ax.plot(solucion[0], solucion[1], 'ro', label='Solución factible')
     plt.legend()
     plt.draw
@@ -112,35 +86,12 @@ def graficar_restricciones(tipo, valores, xlim, ylim, solucion, restricciones_co
     
     plt.show()
 
-
-def main():
-    ingresar_datos()
-    num_restricciones = int(input("Ingrese el número de restricciones: "))
-    restricciones = []
-    tipo = []
-    valores = []
-    cont = []
-    
-    bandera = False
-    p = 0
-    for i in range(num_restricciones):
-        restriccion = input(f"Ingrese la restricción {i + 1} en la forma 'ax + by <= ó >= c': ")
-        restricciones_graph.append(restriccion)
-        coeficientes = list(map(float, restriccion.split()[:5:2]))
-        restricciones.append(coeficientes)
-        tipo.append(restriccion.split()[3])
-        valores.append(float(restriccion.split()[4]))
-        for i in range(len(restriccion) - 1):
-            if restriccion[i:i+2] == ">=":
-                bandera = True
-                cont.append(p)
-        p += 1
-
-    
+def solucionador(restricciones, tipo, valores, cont, c, bandera):
+ 
     xlim = [-50,50]
     ylim = [-50,50]
 
-    c = np.array(list(map(float, input("Ingrese los coeficientes de la función objetivo en la forma 'cx dy': ").split(' '))))
+
     for ress in restricciones:
         ress.pop()
         restricciones_comp.append(ress)
@@ -159,7 +110,7 @@ def main():
 
     print("Punto óptimo: ", res.x)
     print("Valor óptimo: ", -res.fun)
-    print("Valor maximizado: ", res.fun)  # Agregar esta línea
+    print("Valor maximizado: ", res.fun)
 
     punto_optimo = res.x
     valor_optimo = -res.fun
@@ -167,6 +118,55 @@ def main():
     graficar_restricciones(tipo, valores, xlim, ylim, res.x, restricciones_comp)
 
 
-if __name__ == "__main__":
-    main()
-        
+def obtener_datos():
+    try:
+        num_restricciones = int(entry_num_restricciones.get())
+        restricciones = []
+        tipo = []
+        valores = []
+        cont = []
+        bandera = False
+        p = 0
+
+        for i in range(num_restricciones):
+            restriccion = entries_restricciones[i].get()
+            coeficientes = list(map(float, restriccion.split()[:5:2]))
+            restricciones.append(coeficientes)
+            tipo.append(restriccion.split()[3])
+            valores.append(float(restriccion.split()[4]))
+            for i in range(len(restriccion) - 1):
+                if restriccion[i:i+2] == ">=":
+                    bandera = True
+                    cont.append(p)
+            p += 1
+
+        c = np.array(list(map(float, entry_func_objetivo.get().split(' '))))
+        ventana.destroy()
+        solucionador(restricciones, tipo, valores, cont, c, bandera)
+
+    except ValueError:
+        messagebox.showerror("Error", "Por favor, ingrese datos válidos.")
+
+
+ventana = tk.Tk()
+ventana.title("Metodo grafico")
+
+tk.Label(ventana, text="Número de restricciones:\n(Solo ingresar el numero de restricciones a colocar en los recuadros)\n\n\n").grid(row=0, column=0, sticky="w")
+entry_num_restricciones = tk.Entry(ventana)
+entry_num_restricciones.grid(row=0, column=1)
+
+tk.Label(ventana, text="Función objetivo:\n(Ejemplo: 2 3 para 2x + 3y:)\n\n\n").grid(row=1, column=0, sticky="w")
+entry_func_objetivo = tk.Entry(ventana)
+entry_func_objetivo.grid(row=1, column=1)
+
+tk.Label(ventana, text="Restricciones:\n(Ejemplo 2 + 3 <= 8 para 2x + 3y <= 8)").grid(row=2, column=0, sticky="w")
+entries_restricciones = []
+for i in range(10):
+    entry_restriccion = tk.Entry(ventana)
+    entry_restriccion.grid(row=2+i, column=1)
+    entries_restricciones.append(entry_restriccion)
+
+boton_obtener_datos = tk.Button(ventana, text="Obtener datos", command=obtener_datos)
+boton_obtener_datos.grid(row=12, column=1, pady=10)
+
+ventana.mainloop()
